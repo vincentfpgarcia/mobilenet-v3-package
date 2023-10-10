9fac013d2f42bdc97d95d53ec1137b5392672bb7
Vincent Garcia <vincentfpgarcia@gmail.com>
2023-10-10 11:12:00 +0200

    chore: get rid of magic numbers + documentation

--- Sources/MobileNetV3Package/MobileNetV3Package.swift
+++ Sources/MobileNetV3Package/MobileNetV3Package.swift
@@ -6,26 +6,29 @@ import os
 public struct MobileNetV3Package {
     
     private var model: MobileNetV3Model? = nil
+    private var logger = Logger()
 
     public init?(computeUnits: MLComputeUnits = MLComputeUnits.all) {
 
+        // Get the URL of the CoreML model
         guard let url = Bundle.module.url(forResource: "MobileNetV3Model", withExtension: "mlmodelc") else {
+            logger.error("Could not find the CoreML model file")
             return nil
         }
 
+        // Create the configuration for the CoreML model
         let configuration = MLModelConfiguration()
         configuration.computeUnits = computeUnits
 
+        // Load the CoreML model
         guard let model = try? MobileNetV3Model(contentsOf: url, configuration: configuration) else {
+            logger.error("Could not create the CoreML model instance")
             return nil
         }
-
         self.model = model
     }
     
     public func predict(input: UIImage?) -> (label: String?, probability: Double?) {
-
-        let logger = Logger()
         
         // Convert the UIImage into a CGImage
         guard let cgImage = input?.cgImage else {
@@ -45,10 +48,13 @@ public struct MobileNetV3Package {
             return (nil, nil)
         }
 
+        // Number of classes
+        let nbClasses = modelOutput.count
+
         // Argmax computation
-        var maxVal = -1000.0
-        var maxArg = -1
-        for i in 0..<1000 {
+        var maxArg = 0
+        var maxVal = Double(truncating: modelOutput[0])
+        for i in 1..<nbClasses {
             let val = Double(truncating: modelOutput[i])
             if val > maxVal {
                 maxVal = val
@@ -61,7 +67,7 @@ public struct MobileNetV3Package {
 
         // Compute the probability (softmax value)
         var sumExp = 0.0
-        for i in 0..<1000 {
+        for i in 0..<nbClasses {
             sumExp += exp(Double(truncating: modelOutput[i]))
         }
         let proba = exp(Double(truncating: modelOutput[maxArg])) / sumExp
